d17f9bbe2332ce38ee4975b5744da18ab4f863c0
syedusman-create <syed18usman18@gmail.com>
2025-04-23 15:11:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def turn_into_story(summary):
     return model.invoke(prompt)
 
 # Streamlit UI
-st.title("📺 PragyanAI - YouTube/Uploaded Video Summarizer Using Groq LLM")
+st.title("📺 Dichi Sallu - YouTube/Uploaded Video Summarizer Using Groq LLM")
 #st.image("PragyanAI_Transperent.png")
 
 youtube_url = st.text_input("Paste a YouTube video URL:", placeholder="https://www.youtube.com/watch?v=example")
